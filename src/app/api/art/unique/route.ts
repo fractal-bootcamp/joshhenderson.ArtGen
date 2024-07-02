a5c7d2127598affc93263c4bf95ff093d358b0fd
Joshua Henderson-Jardim <hendersonjardimj@gmail.com>
2024-07-02 12:07:31 -0400

    this better work now

--- src/app/api/art/unique/route.ts
+++ src/app/api/art/unique/route.ts
@@ -1,13 +1,14 @@
-import { NextApiRequest, NextApiResponse } from "next";
 import prisma from "@/app/Prisma-Client";
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
-    const { id } = req.query;
-    const art = await prisma.art.findUnique({
-        where: {
-            id: String(id)
-        }
-    })
-    // return Response.json({ message: 'Hello, I am /api/art/all !' });
-    return Response.json(art);
-} 
+
+export async function GET(req: NextRequest, res: NextResponse) { }
+
+//     // const art = await prisma.art.findUnique({
+//     //     where: {
+//     //         id: String(id)
+//     //     }
+//     // })
+//     // return Response.json({ message: 'Hello, I am /api/art/all !' });
+//     return Response.json(art);
+// } 
