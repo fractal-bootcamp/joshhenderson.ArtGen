00acf4e780b943b95c7aacc28632baa7e49cd5f5
Joshua Henderson-Jardim <hendersonjardimj@gmail.com>
2024-07-04 20:47:41 -0400

    refactor and added 3d rending

--- src/app/components/ArtRender.tsx
+++ src/app/components/ArtRender.tsx
@@ -1,23 +1,12 @@
-function ArtRender({ title, description, background, setTitle, setDescription, setBackground }: { title: string, description: string, background: string, setTitle: (title: string) => void, setDescription: (description: string) => void, setBackground: (background: string) => void }) {
-    function submissionHandler() {
-        console.log("Submitting your art...")
-        // console.log({ title, description, background })
-        fetch('/api/artPosts', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ title: title, description: description, background: background }),
-        })
-    }
+function ArtRender({ title, description, background }: { title: string, description: string, background: string }) {
+
     return (
         <>
-            <div>MakeArt</div>
-            <div className={`bg-${background} h-80 w-80`}></div>
-            <input className='text-black' type="text" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)}></input>
-            <input className='text-black' type="text" placeholder="Description" value={description} onChange={(e) => setDescription(e.target.value)}></input>
-            <input className='border-black border-2 h-4 w-6' type="color" value={background} onChange={(e) => setBackground(e.target.value)}></input>
-            <button onClick={() => { (submissionHandler()) }}>Submit your art!</button>
+            <title>
+                {title}
+            </title>
+            <div>{description}</div>
+            <div className="h-80 w-80" style={{ backgroundColor: background }}></div>
         </>
     )
 }
--- src/app/makeArt/page.tsx
+++ src/app/makeArt/page.tsx
@@ -3,31 +3,21 @@
 import { User } from "@prisma/client";
 import { Component, useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import ArtRender from "../components/ArtRender";
+import Controls from "../components/Controls";
 
 
-function ArtRender({ title, description, background }: { title: string, description: string, background: string }): JSX.Element {
-
-    return (
-        <>
-            <div>MakeArt</div>
-            <div className={`bg-${background} h-80 w-80`}></div>
-            {/* <input className='text-black' type="text" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)}></input>
-            <input className='text-black' type="text" placeholder="Description" value={description} onChange={(e) => setDescription(e.target.value)}></input>
-            <input className='border-black border-2 h-4 w-6' type="color" value={background} onChange={(e) => setBackground(e.target.value)}></input>
-            <button onClick={() => { (submissionHandler()) }}>Submit your art!</button> */}
-        </>
-    )
-}
 export default function MakeArt() {
     const [title, setTitle] = useState("")
     const [description, setDescription] = useState("")
     const [background, setBackground] = useState("white")
     const router = useRouter()
-    function submissionHandler() {
+
+    async function submissionHandler() {
 
         console.log("Submitting your art...")
         console.log({ title, description, background })
-        fetch('/api/artPosts', {
+        await fetch('/api/artPosts', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -44,15 +34,14 @@ export default function MakeArt() {
     return (
         <>
             <div>MakeArt</div>
-            <div className={`bg-${background} h-80 w-80`}></div>
-            <input className='text-black' type="text" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)}></input>
-            <input className='text-black' type="text" placeholder="Description" value={description} onChange={(e) => setDescription(e.target.value)}></input>
-            <input className='border-black border-2 h-4 w-6' type="color" value={background} onChange={(e) => setBackground(e.target.value)}></input>
-            <button onClick={submissionHandler}>Submit your art!</button>
+
+            <ArtRender title={title} description={description} background={background} />
 
             <div className="flex flex-col items-center justify-center h-screen">
-                <ArtRender key="2" title={title} description={description} background={background} />
+                {console.log('background', background)}
+                <Controls title={title} description={description} background={background} setTitle={setTitle} setDescription={setDescription} setBackground={setBackground} />
             </div>
+            <button onClick={submissionHandler}>Submit your art!</button>
         </>
     );
 }
